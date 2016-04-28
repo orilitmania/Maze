31cc8a3bd95aa01584b5422c346bef6fcb25e98d
lrevach <lrevach@gmail.com>
2016-04-28 11:31:57 +0300

    Update BestFS.java

--- java proj/src/algorithms/search/BestFS.java
+++ java proj/src/algorithms/search/BestFS.java
@@ -1,12 +1,31 @@
 package algorithms.search;
 
 
+import java.util.PriorityQueue;
+import java.util.Queue;
 
+/**
+ * The Class BestFS extends BFS with the use of states costs.
+ * i decided to use an this way to build the bestFS because its much more generic flexible and effortless this way.
+ * its more efficient clean when looking at it from a design point of view.
+ *@author liron
+ * @param <T> the generic type
+ */
 public class BestFS<T> extends BFS<T> {
 	
+	/* (non-Javadoc)
+	 * @see algorithms.search.BFS#calculateCost(algorithms.search.State, algorithms.search.State, algorithms.search.Searchable)
+	 */
 	@Override
-	public double calculateCost(State<T> currState,State<T> neighbor,Searchable<T> s){
+	public double calculateCost(State<T> previousState,State<T> newState,Searchable<T> s){
 		
-		return currState.getCost()+ s.getCostForStates(currState, neighbor);
+		return previousState.getCost()+ s.getCostForStates(previousState, newState);
 	}
+	
+	public Queue<State<T>> getQueue(){
+			Queue<State<T>> q = new PriorityQueue<State<T>>();
+			return q;
+	}
+	
 }
+
